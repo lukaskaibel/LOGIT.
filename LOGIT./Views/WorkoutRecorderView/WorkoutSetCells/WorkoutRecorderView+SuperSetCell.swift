3eff7af0e4d22cd3ce0eedf54142507f29d8f1a3
Lukas <luks007@web.de>
2022-12-28 00:31:43 +0100

    SuperSetCell changes

--- LOGIT./Views/WorkoutRecorderView/WorkoutSetCells/WorkoutRecorderView+SuperSetCell.swift
+++ LOGIT./Views/WorkoutRecorderView/WorkoutSetCells/WorkoutRecorderView+SuperSetCell.swift
@@ -9,29 +9,41 @@ import SwiftUI
 
 extension WorkoutRecorderView {
     
-    internal func SuperSetCell(for superSet: SuperSet) -> some View {
+    internal func superSetCell(for superSet: SuperSet) -> some View {
         VStack {
             HStack {
                 Text("1")
                     .foregroundColor(.secondaryLabel)
                     .font(.footnote)
-                SetEntryEditor(repetitions: Binding(get: { superSet.repetitionsFirstExercise },
-                                                         set: { superSet.repetitionsFirstExercise = $0 }),
-                                    weight: Binding(get: { superSet.weightFirstExercise },
-                                                    set: { superSet.weightFirstExercise = $0 }),
-                                    repetitionsPlaceholder: repetitionsPlaceholder(for: superSet).first!,
-                                    weightPlaceholder: weightsPlaceholder(for: superSet).first!)
+                SetEntryEditor(
+                    repetitions: Binding(
+                        get: { superSet.repetitionsFirstExercise },
+                        set: { superSet.repetitionsFirstExercise = $0 }
+                    ),
+                    weight: Binding(
+                        get: { superSet.weightFirstExercise },
+                        set: { superSet.weightFirstExercise = $0 }
+                    ),
+                    repetitionsPlaceholder: repetitionsPlaceholder(for: superSet).first!,
+                    weightPlaceholder: weightsPlaceholder(for: superSet).first!
+                )
             }
             HStack {
                 Text("2")
                     .foregroundColor(.secondaryLabel)
                     .font(.footnote)
-                SetEntryEditor(repetitions: Binding(get: { superSet.repetitionsSecondExercise },
-                                                         set: { superSet.repetitionsSecondExercise = $0 }),
-                                    weight: Binding(get: { superSet.weightSecondExercise },
-                                                    set: { superSet.weightSecondExercise = $0 }),
-                                    repetitionsPlaceholder: repetitionsPlaceholder(for: superSet).second!,
-                                    weightPlaceholder: weightsPlaceholder(for: superSet).second!)
+                SetEntryEditor(
+                    repetitions: Binding(
+                        get: { superSet.repetitionsSecondExercise },
+                        set: { superSet.repetitionsSecondExercise = $0 }
+                    ),
+                    weight: Binding(
+                        get: { superSet.weightSecondExercise },
+                        set: { superSet.weightSecondExercise = $0 }
+                    ),
+                    repetitionsPlaceholder: repetitionsPlaceholder(for: superSet).second!,
+                    weightPlaceholder: weightsPlaceholder(for: superSet).second!
+                )
             }.accentColor(superSet.secondaryExercise?.muscleGroup?.color)
         }
     }
