312eb2fe7211b3eeaf2158fb6f4dcb6834e8ff46
Lukas <luks007@web.de>
2023-07-31 02:14:35 +0200

    🛠️  Added gitignore

---
+++ LOGIT./Environment/EnvironmentValues+.swift
@@ -0,0 +1,35 @@
+//
+//  EnvironmentValues+.swift
+//  LOGIT.
+//
+//  Created by Lukas Kaibel on 29.07.23.
+//
+
+import SwiftUI
+
+private struct WorkoutSetTemplateSetDictionaryKey: EnvironmentKey {
+    static let defaultValue: [WorkoutSet: TemplateSet] = [WorkoutSet: TemplateSet]()
+}
+
+private struct SetWorkoutEndDateKey: EnvironmentKey {
+    static let defaultValue: (Date) -> Void = { _ in }
+}
+
+private struct CanEditKey: EnvironmentKey {
+    static let defaultValue: Bool = true
+}
+
+extension EnvironmentValues {
+    var workoutSetTemplateSetDictionary: [WorkoutSet: TemplateSet] {
+        get { self[WorkoutSetTemplateSetDictionaryKey.self] }
+        set { self[WorkoutSetTemplateSetDictionaryKey.self] = newValue }
+    }
+    var setWorkoutEndDate: (Date) -> Void {
+        get { self[SetWorkoutEndDateKey.self] }
+        set { self[SetWorkoutEndDateKey.self] = newValue }
+    }
+    var canEdit: Bool {
+        get { self[CanEditKey.self] }
+        set { self[CanEditKey.self] = newValue }
+    }
+}
