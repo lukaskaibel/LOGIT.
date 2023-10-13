00cfb79306d1e48d1708fdd454aa6efadb698bcb
Lukas <luks007@web.de>
2023-10-13 14:26:53 +0200

    ✨ Added Subscription Button in Settings

--- LOGIT./UI/Screens/SettingsScreen.swift
+++ LOGIT./UI/Screens/SettingsScreen.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsScreen: View {
+    
+    @EnvironmentObject private var purchaseManager: PurchaseManager
 
     // MARK: - UserDefaults
 
@@ -15,49 +18,112 @@ struct SettingsScreen: View {
     @AppStorage("workoutPerWeekTarget") var workoutPerWeekTarget: Int = 3
     @AppStorage("preventAutoLock") var preventAutoLock: Bool = true
     @AppStorage("timerIsMuted") var timerIsMuted: Bool = false
+    
+    // MARK: - State
+    
+    @State private var isShowingUpgradeToPro = false
 
     // MARK: - Body
 
     var body: some View {
-        List {
-            Section {
-                Picker(
-                    NSLocalizedString("targetPerWeek", comment: ""),
-                    selection: $workoutPerWeekTarget
-                ) {
-                    ForEach(1..<10, id: \.self) { i in
-                        Text(String(i)).tag(i)
+        ScrollView {
+            VStack(spacing: SECTION_SPACING) {
+                VStack(spacing: CELL_SPACING) {
+                    HStack {
+                        Text(NSLocalizedString("targetPerWeek", comment: ""))
+                        Spacer()
+                        Picker(
+                            NSLocalizedString("targetPerWeek", comment: ""),
+                            selection: $workoutPerWeekTarget
+                        ) {
+                            ForEach(1..<10, id: \.self) { i in
+                                Text(String(i)).tag(i)
+                            }
+                        }
+                    }
+                    .padding(CELL_PADDING)
+                    .tileStyle()
+                    HStack {
+                        Text(NSLocalizedString("unit", comment: ""))
+                        Spacer()
+                        Picker(
+                            NSLocalizedString("unit", comment: ""),
+                            selection: $weightUnit,
+                            content: {
+                                Text("kg").tag(WeightUnit.kg)
+                                Text("lbs").tag(WeightUnit.lbs)
+                            }
+                        )
+                    }
+                    .padding(CELL_PADDING)
+                    .tileStyle()
+                }
+                
+                VStack(spacing: CELL_SPACING) {
+                    VStack(alignment: .leading) {
+                        Toggle(NSLocalizedString("preventAutoLock", comment: ""), isOn: $preventAutoLock)
+                        Text(NSLocalizedString("preventAutoLockDescription", comment: ""))
+                            .font(.caption)
+                            .foregroundStyle(.secondary)
+                    }
+                    .padding(CELL_PADDING)
+                    .tileStyle()
+                    Toggle(NSLocalizedString("timerIsMuted", comment: ""), isOn: $timerIsMuted)
+                        .padding(CELL_PADDING)
+                        .tileStyle()
+                }
+                
+                VStack(spacing: SECTION_HEADER_SPACING) {
+                    Text(NSLocalizedString("subscription", comment: ""))
+                        .sectionHeaderStyle2()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    VStack(spacing: CELL_SPACING) {
+                        Button {
+                            if purchaseManager.hasUnlockedPro {
+                                Task {
+                                    if let window = UIApplication.shared.connectedScenes.first {
+                                       do {
+                                           try  await AppStore.showManageSubscriptions(in: window as! UIWindowScene)
+                                       } catch {
+                                           print("Error:(error)")
+                                       }
+                                   }
+                                }
+                            } else {
+                                isShowingUpgradeToPro = true
+                            }
+                        } label: {
+                            if purchaseManager.hasUnlockedPro {
+                                Text(NSLocalizedString("showSubscriptions", comment: ""))
+                            } else {
+                                HStack {
+                                    Image(systemName: "crown.fill")
+                                    Text(NSLocalizedString("upgradeTo", comment: ""))
+                                    LogitProLogo()
+                                        .environment(\.colorScheme, .light)
+                                }
+                            }
+                        }
+                        .buttonStyle(BigButtonStyle())
                     }
                 }
-                Picker(
-                    NSLocalizedString("unit", comment: ""),
-                    selection: $weightUnit,
-                    content: {
-                        Text("kg").tag(WeightUnit.kg)
-                        Text("lbs").tag(WeightUnit.lbs)
-                    }
-                )
             }
-            Section {
-                Toggle(NSLocalizedString("preventAutoLock", comment: ""), isOn: $preventAutoLock)
-            } footer: {
-                Text(NSLocalizedString("preventAutoLockDescription", comment: ""))
-            }
-            Section {
-                Toggle(NSLocalizedString("timerIsMuted", comment: ""), isOn: $timerIsMuted)
-            }
+            .padding(.horizontal)
         }
-        .listStyle(.insetGrouped)
         .navigationTitle(NSLocalizedString("settings", comment: ""))
         .navigationBarTitleDisplayMode(.large)
+        .sheet(isPresented: $isShowingUpgradeToPro) {
+            UpgradeToProScreen()
+        }
     }
 
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
+        NavigationStack {
             SettingsScreen()
         }
+        .environmentObject(PurchaseManager(entitlementManager: EntitlementManager()))
     }
 }
